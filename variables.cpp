630d4084a411b1641262fdfdf3b2a25161acedf6
SRIJAL SRIVASTAVA <143055065+SrijalSrivastava19@users.noreply.github.com>
2026-06-07 16:32:03 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 10; // this is an integer variable
+    float b = 5.5; // this is a floating-point variable
+    char c = 'A'; // this is a character variable
+    bool d = true; // this is a boolean variable
+    int age;
+    cout <<"Enter your age: "<< endl; //prompting user to enter age
+    cin >> age; //taking input from user for age
+    cout << "your age  is : " << age << endl;
+    cout <<a << endl;
+    cout <<b << endl;
+    cout <<c << endl;
+    cout <<d << endl;
+    return 0;
+}
