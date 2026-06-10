e0aa46888b8f4ca1307747725aa9690a235a7334
SRIJAL SRIVASTAVA <143055065+SrijalSrivastava19@users.noreply.github.com>
2026-06-10 22:13:35 +0530

    Add files via upload

--- sum.cpp
+++ sum.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
-int main (){
-    int num1 , num2  , sum;
-    cout<<"Enter fisrt number "<< endl;
-    cin >> num1;
-    cout << "Enter your second number "<< endl;
-    cin >> num2;
-    sum = num1+num2;
-    cout<< "The sum of numbers are :" << sum << endl;
+int main(){
+    int num=0 , sum=0;
+    cout <<"Enter the number : " <<endl;
+    cin>>num;
+    for(int i=1; i<=num ; i++)
+    {
+        cout << i << " ";
+        sum = sum+i;
+
+    }
+    
+    cout << "Sum = "<< sum << endl;
     return 0;
 }
