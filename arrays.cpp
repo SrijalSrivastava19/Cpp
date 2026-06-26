d50f835de0c9df9939a73f3e9c499e831c675da1
SRIJAL SRIVASTAVA <143055065+SrijalSrivastava19@users.noreply.github.com>
2026-06-26 17:58:15 +0530

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main () {
+    //array name -----> marks 
+    // sq bracket contains size of array or we can leave it empty 
+    int marks[5]= {99,100,54,36,88};
+    double price[] ={ 99.99 , 100 , 150};
+    cout<< marks[0] << endl;
+    cout<< marks[1] << endl;
+    cout<< marks[3] << endl;
+    cout<< price[0] << endl;
+    // size --> 0 to size-1
+    return 0;
+}
